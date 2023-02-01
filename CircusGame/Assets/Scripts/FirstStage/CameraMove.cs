38f2ef5ba8c4b9c7d7308baff06c2b255303c985
qkqakseb <parknanju6376@gmail.com>
2023-02-01 23:46:27 +0900

    add make cameraScript

--- CircusGame/Assets/Scripts/FirstStage/CameraMove.cs
+++ CircusGame/Assets/Scripts/FirstStage/CameraMove.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    Vector3 camerPostion = new Vector3(0, 0, -100);
+    Vector3 cameraPostion = new Vector3(0, 0, -100);
     
-    private float playTransform = default;
+    private Transform playTransform = default;
     // 카메라가 플레이어를 따라가야된다.
 
     // Start is called before the first frame update
     void Start()
     {
-        playTransform = gameObject.GetComponentMust<Rigidbody2D>();
+        playTransform = gameObject.cameraPosition.GetComponentMust<Transform>();
     }
 
     // Update is called once per frame
